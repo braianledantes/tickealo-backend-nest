0f7825df4ebbfabaf583475c8c5cb71a90353f0c
Braian Ledantes <braianledantes@gmail.com>
2025-10-29 16:16:45 -0300

    feat(countries): add public endpoints for country info and currency

--- src/countries/countries.controller.ts
+++ src/countries/countries.controller.ts
@@ -1,20 +1,24 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { CountriesService } from './countries.service';
+import { Public } from '../auth/decorators/public.decorator';
 
 @Controller('countries')
 export class CountriesController {
   constructor(private readonly countryInfoService: CountriesService) {}
 
+  @Public()
   @Get(':iso')
   async getByIso(@Param('iso') iso: string) {
     return this.countryInfoService.getFullCountryInfoByISO(iso);
   }
 
+  @Public()
   @Get(':iso/currency')
   async getCurrency(@Param('iso') iso: string) {
     return this.countryInfoService.getCurrencyByISO(iso);
   }
 
+  @Public()
   @Get()
   async getAllCountries() {
     return this.countryInfoService.getListOfCountriesNamesByName();
