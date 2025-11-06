c2cc00f8f019d7e6235b7aa34d157304b6f564d0
Braian Ledantes <braianledantes@gmail.com>
2025-11-05 22:19:32 -0300

    fix(job-compras): Actualiza el stock del evento asociado a la entrada de los tickets eliminados

--- src/compras/services/jobs-compras.service.ts
+++ src/compras/services/jobs-compras.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 import { Entrada } from 'src/eventos/entities/entrada.entity';
+import { Evento } from 'src/eventos/entities/evento.entity';
 import { Ticket } from 'src/tickets/entities/ticket.entity';
 import { DataSource, In, LessThan } from 'typeorm';
 import { Compra } from '../entities/compra.entity';
@@ -8,28 +9,37 @@ import { EstadoCompra } from '../enums/estado-compra.enum';
 
 @Injectable()
 export class JobsComprasService {
+  private readonly logger = new Logger(JobsComprasService.name);
+
   constructor(private readonly dataSource: DataSource) {}
 
   /**
    * Elimina las compras que están en estado INICIADA y que fueron creadas hace más de 1 hora.
-   * Este job se ejecuta cada media hora.
+   * Este job se ejecuta cada 1 minuto.
    */
-  @Cron('0 */30 * * * *')
+  @Cron('* * * * *')
   async eliminarComprasNoFinalizadas() {
     const tiempoLimite = 60 * 60 * 1000;
-    const fechaLimite = new Date(Date.now() - tiempoLimite);
 
     const queryRunner = this.dataSource.createQueryRunner();
     await queryRunner.connect();
     await queryRunner.startTransaction();
+
     try {
+      // obtengo la fecha límite
+      const resNow: { now: Date }[] = await this.dataSource.manager.query(
+        'SELECT NOW() AS now',
+      );
+      const now = resNow[0].now.getTime();
+      const fechaLimite = new Date(now - tiempoLimite);
+
       // Primero verificar cuántas compras INICIADA hay en total
       const comprasNoFinalizadas = await queryRunner.manager.find(Compra, {
         where: {
           estado: EstadoCompra.INICIADA,
           createdAt: LessThan(fechaLimite),
         },
-        relations: ['tickets', 'tickets.entrada'],
+        relations: ['tickets', 'tickets.entrada', 'tickets.entrada.evento'],
       });
 
       // elimina los tickets
@@ -56,13 +66,36 @@ export class JobsComprasService {
           }
         }
 
+        // Actualiza el stock del evento asociado a la entrada de los tickets eliminados
+        await queryRunner.manager.increment(
+          Evento,
+          { id: compra.tickets[0].entrada.evento.id },
+          'stockEntradas',
+          compra.tickets.length,
+        );
+
         // elimina la compra
         await queryRunner.manager.delete(Compra, { id: compra.id });
       }
-
       await queryRunner.commitTransaction();
+
+      if (comprasNoFinalizadas.length > 0) {
+        const cantTicketsEliminados = comprasNoFinalizadas.reduce(
+          (sum, compra) => sum + compra.tickets.length,
+          0,
+        );
+
+        this.logger.log(
+          `Se eliminaron ${comprasNoFinalizadas.length} compras no finalizadas y ${cantTicketsEliminados} tickets asociados.`,
+        );
+      }
     } catch (error) {
       await queryRunner.rollbackTransaction();
+      if (error instanceof Error) {
+        this.logger.error(
+          `Error al eliminar compras no finalizadas: ${error.message}`,
+        );
+      }
       throw error;
     } finally {
       await queryRunner.release();
